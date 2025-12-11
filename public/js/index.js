50bd787ea2cd3e46227926a03dac6644e3dee853
Migmig33 <migztan66@gmail.com>
2025-12-11 10:58:59 +0800

    addedd script for 'forget password'

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+const forget = document.getElementById("forget");
+forget.addEventListener('click', function(){
+    ShowAlert();
+});
+
+function ShowAlert(){
+    alert("Send your Registered Employee Number and your Full Name to migztan66@gmail.com to renew your password");
+}
